c0bb6edd86bae30ef4674e4c995756885bcfe82a
DAS <adoulgou@gmail.com>
2025-06-30 11:33:02 +0000

    Blocked cross-origin request from 3000-firebase-studio-1750673315832.clu

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ const nextConfig = {
   },
   allowedDevOrigins: [
     'https://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev',
-    'http://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev'
+    'http://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev',
+    'http://localhost:3000',
   ],
 };
 
