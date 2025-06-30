b183c7eb3ef58b0a59c7af0a419b32f7ea7ccac3
DAS <adoulgou@gmail.com>
2025-06-30 10:28:04 +0000

    ✓ Compiled in 1987ms (465 modules)  ○ Compiling /favicon.ico ...  ⚠ Bloc

--- next.config.js
+++ next.config.js
@@ -22,7 +22,10 @@ const nextConfig = {
       },
     ],
   },
-  allowedDevOrigins: ['https://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev'],
+  allowedDevOrigins: [
+    'https://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev',
+    'http://3000-firebase-studio-1750673315832.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev'
+  ],
   webpack: (config) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
     config.module.rules.push({
