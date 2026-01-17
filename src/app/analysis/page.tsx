35f5e8f10329225e51b8d549f5d620dba8957dd8
DAS <adoulgou@gmail.com>
2026-01-17 10:46:12 +0000

    J'ai activé l'authentification par téléphone

--- src/app/analysis/page.tsx
+++
@@ -1,7 +0,0 @@
-// This file is intentionally left empty and can be deleted.
-// The AI Analysis feature has been removed as per user request.
-import { redirect } from 'next/navigation';
-
-export default function AnalysisPage() {
-  redirect('/');
-}
--- src/app/invoices/RecordPaymentButton.tsx
+++
@@ -1,4 +0,0 @@
-
-// This component has been removed and its functionality is now handled by the 'Settlements' page.
-// This file can be deleted.
-export {};
