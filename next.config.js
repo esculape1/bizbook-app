6b8aad4d34342ab029e490a4ab06909e2eeea742
DAS <adoulgou@gmail.com>
2026-01-22 13:27:29 +0000

    j'ai aussi remarquer que le demarrage de firebase studio est lent ça beu

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
   async redirects() {
     return [
       {
